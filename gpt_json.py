c109ee4227a15310e1d38e9687de885a95d9c697
Synthetics Code <>
2023-01-07 14:08:38 -0600

    expose only the respond_json

--- gpt_json.py
+++ gpt_json.py
@@ -2,33 +2,6 @@ import json
 import gpt_api
 
 
-# create a class that can be initialized with a dict
-class Person:
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}({self.__dict__})"
-
-    def __str__(self):
-        return f"{self.__class__.__name__}({self.__dict__})"
-
-    def __eq__(self, other):
-        return self.__dict__ == other.__dict__
-
-
-# really nice that it is a felxible dict and can be used for any json
-bio_json_format = {
-    "birth_date": "1999-01-01",
-    "death_date": "2021-01-01",
-    "cause_of_death": "",
-    "name": "",
-    "best_known_for": "",
-    "greatness_rate_from_1_to_10": 0,
-    "nationality": "",
-}
-
 # returns a dict given a topic and a json format to follow
 # tries_left is used to try again if the response is not a valid json
 def respond_json(topic: str, json_format: dict, tries_left: int = 2) -> dict:
@@ -52,6 +25,32 @@ def respond_json(topic: str, json_format: dict, tries_left: int = 2) -> dict:
 
 
 if __name__ == "__main__":
+    # really nice that it is a felxible dict and can be used for any json
+    bio_json_format = {
+        "birth_date": "1999-01-01",
+        "death_date": "2021-01-01",
+        "cause_of_death": "",
+        "name": "",
+        "best_known_for": "",
+        "greatness_rate_from_1_to_10": 0,
+        "nationality": "",
+    }
+
+    # create a class that can be initialized with a dict
+    class Person:
+        def __init__(self, **kwargs):
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+        def __repr__(self):
+            return f"{self.__class__.__name__}({self.__dict__})"
+
+        def __str__(self):
+            return f"{self.__class__.__name__}({self.__dict__})"
+
+        def __eq__(self, other):
+            return self.__dict__ == other.__dict__
+
     # test
     alexander = Person(**respond_json("Alexander the great", bio_json_format))
     print(alexander)
